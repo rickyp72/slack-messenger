ba20eda1e4c7717f71959fa761f9063a04b685fc
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2020-05-31 22:35:36 +0100

    working secrets manager

--- getSecret.js
+++ getSecret.js
@@ -2,70 +2,64 @@
 
 
 exports.getSecret = function (smpath) {
-    let jsonData = '';
-    console.log("SM PATH: " + smpath)
-    // Load the AWS SDK
 
-    var AWS = require('aws-sdk'),
-        region = "eu-west-2",
-        secretName = smpath,
-        secret,
-        decodedBinarySecret;
 
+    return new Promise((resolve) => {
+        let jsonData = '';
+        console.log("SM PATH: " + smpath)
+        // Load the AWS SDK
+
+        var AWS = require('aws-sdk'),
+            region = "eu-west-2",
+            secretName = smpath,
+            secret,
+            decodedBinarySecret;
 
 
 // Create a Secrets Manager client
-    var client = new AWS.SecretsManager({
-        region: region
-    });
-    
+        var client = new AWS.SecretsManager({
+            region: region
+        });
 
-    client.getSecretValue({SecretId: secretName}, function (err, data) {
-        console.log("SecretName: " + secretName)
-        if (err) {
-            if (err.code === 'DecryptionFailureException')
-                // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-                // Deal with the exception here, and/or rethrow at your discretion.
-                throw err;
-            else if (err.code === 'InternalServiceErrorException')
-                // An error occurred on the server side.
-                // Deal with the exception here, and/or rethrow at your discretion.
-                throw err;
-            else if (err.code === 'InvalidParameterException')
-                // You provided an invalid value for a parameter.
-                // Deal with the exception here, and/or rethrow at your discretion.
-                throw err;
-            else if (err.code === 'InvalidRequestException')
-                // You provided a parameter value that is not valid for the current state of the resource.
-                // Deal with the exception here, and/or rethrow at your discretion.
-                throw err;
-            else if (err.code === 'ResourceNotFoundException')
-                // We can't find the resource that you asked for.
-                // Deal with the exception here, and/or rethrow at your discretion.
-                throw err;
-        }
-        else {
-            // Decrypts secret using the associated KMS CMK.
-            // Depending on whether the secret is a string or binary, one of these fields will be populated.
-            if ('SecretString' in data) {
-                secret = data.SecretString;
+
+        client.getSecretValue({SecretId: secretName}, function (err, data) {
+            console.log("SecretName: " + secretName)
+            if (err) {
+                if (err.code === 'DecryptionFailureException')
+                    // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+                    // Deal with the exception here, and/or rethrow at your discretion.
+                    throw err;
+                else if (err.code === 'InternalServiceErrorException')
+                    // An error occurred on the server side.
+                    // Deal with the exception here, and/or rethrow at your discretion.
+                    throw err;
+                else if (err.code === 'InvalidParameterException')
+                    // You provided an invalid value for a parameter.
+                    // Deal with the exception here, and/or rethrow at your discretion.
+                    throw err;
+                else if (err.code === 'InvalidRequestException')
+                    // You provided a parameter value that is not valid for the current state of the resource.
+                    // Deal with the exception here, and/or rethrow at your discretion.
+                    throw err;
+                else if (err.code === 'ResourceNotFoundException')
+                    // We can't find the resource that you asked for.
+                    // Deal with the exception here, and/or rethrow at your discretion.
+                    throw err;
             } else {
-                let buff = new Buffer(data.SecretBinary, 'base64');
-                decodedBinarySecret = buff.toString('ascii');
+                // Decrypts secret using the associated KMS CMK.
+                // Depending on whether the secret is a string or binary, one of these fields will be populated.
+                if ('SecretString' in data) {
+                    secret = data.SecretString;
+                    console.log("SECRET: " + secret)
+                    resolve(secret)
+                } else {
+                    let buff = new Buffer(data.SecretBinary, 'base64');
+                    decodedBinarySecret = buff.toString('ascii');
+                    resolve(decodedBinarySecret)
+                    console.log("FALLOUT")
+                }
             }
-        }
-
-        // Your code goes here.
-        // console.log("SECRET INSIDE: " + secret)
-        //
-        jsonData = JSON.parse(secret);
-        // return jsonData
-        console.log("JSONDATA: " + jsonData.hookPath)
-        //
-        // return jsonData.hookPath
-    });
-    console.log("OUTSIDE SECRET: " + jsonData)
-    // var jsonData = JSON.parse(secret)
-    return jsonData.hookPath;
+        });
+    })
 }
 
--- handler.js
+++ handler.js
@@ -5,20 +5,17 @@ const secret = require('./getSecret.js')
 
 exports.handler = async (event) => {
     // TODO implement
-
-
-    // var myHook = await secret.getSecret(event.webhook)
+    console.log("EVENT WEBHOOK: " + event.webhook)
     let myHook = await secret.getSecret(event.webhook)
-    // let myHook = await secret.getSecret(event.webhook).promise();
 
-    // var myHook = secret.getSecret('dev/nhs/slack/hookPath') // test setting webhook
-    // let myHook = secret.getSecret(event.webhook)
-    console.log("MYHOOK: " + myHook)
+    var jsonData = JSON.parse(myHook);
+
+    console.log("MYHOOK AS JSON: " + jsonData.hookPath)
 
     let slackmsg = event.report;
-    let webhook = myHook;     // use secret manager
+    let webhook = jsonData.hookPath;     // use secret manager
     // let webhook = event.webhook // use plaintext webhook for testing
-    console.log(webhook)
+    console.log("WEBHOOK: " + webhook)
     return pts.posttoslack("hooks.slack.com", webhook, slackmsg);
 
 };
