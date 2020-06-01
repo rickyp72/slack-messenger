f5da6401e7e67a2a6cc162f7d19071cc6bf1945f
R Parkinson <rickyp72@gmail.com>
2020-06-01 17:31:01 +0100

    Allow for secret manager or plaintext webhook event

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ sls deploy -v
 
 sls logs -f slack -t
  
-sls invoke -f slack --path mocks/slack-webhook-event.json
+sls invoke -f slack -p mocks/slack-webhook-event.json
  
-sls invoke -f slack --path mocks/slack-secretmanager-event.json
+sls invoke -f slack -p mocks/slack-secretmanage-event.json
  ```
--- handler.js
+++ handler.js
@@ -2,22 +2,22 @@
 const pts = require('./posttoslack.js')
 const secret = require('./getSecret.js')
 
-
-
 exports.handler = async (event) => {
-    // TODO implement
-    console.log("EVENT WEBHOOK: " + event.webhook)
-    let myHook = await secret.getSecret(event.webhook)
+    let webhook;
+    if ( event.webhook.startsWith("/services/") ) {
+        webhook = event.webhook // use plaintext webhook for testing
+        console.log("WEBHOOK: " + webhook)
+        console.log("TRUE")
+    } else {
+        let myHook = await secret.getSecret(event.webhook)
+        let jsonData = JSON.parse(myHook);
+        console.log("MYHOOK AS JSON: " + jsonData.hookPath)
 
-    let jsonData = JSON.parse(myHook);
-
-    console.log("MYHOOK AS JSON: " + jsonData.hookPath)
-
+        webhook = jsonData.hookPath;     // use secret manager
+        console.log("WEBHOOK: " + webhook)
+        console.log("FALSE")
+    }
     let slackmsg = event.report;
-    let webhook = jsonData.hookPath;     // use secret manager
-    // let webhook = event.webhook // use plaintext webhook for testing
-    console.log("WEBHOOK: " + webhook)
     return pts.posttoslack("hooks.slack.com", webhook, slackmsg);
-
 };
 
